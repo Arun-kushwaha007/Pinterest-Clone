49acb9297a3a1cb9c1ae47d35175abaf35886c6d
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-05-16 11:25:35 +0530

    uploading done

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./posts");
 const passport = require("passport");
+const upload = require('./multer');
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 /* GET home page. */
@@ -10,16 +11,38 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login',{error: req.flash('error')}); /// the error portion will provide us an array of error .
 });
 
 router.get('/feed', function(req, res, next) {
   res.render('feed');
 });
 
+router.post('/upload', isLoggedIn ,upload.single("file") ,async function(req, res, next) {
+  if(!req.file){
+    return res.status(400).send("NO file were uploaded");
+  }
+  // res.send('file uploaded successfully!')
+  // jo file upload hui hai use save karro as a post and uska post id user ko do and post ko userid do.
+  const user = await userModel.findOne({username: req.session.passport.user});
+  const post = await postModel.create({
+    image: req.file.filename,
+    imageText : req.body.filecaption,
+    user: user._id
+  });
+  user.posts.push(post._id);
+  await user.save(); 
+  res.redirect("/profile");
+});
+
 // profile router 
-router.get('/profile',isLoggedIn, function(req, res, next) {
-  res.render("profile");
+router.get('/profile',isLoggedIn, async function(req, res, next) {
+    const user = await userModel.findOne({
+      username: req.session.passport.user
+    })
+    .populate("posts");
+    console.log(user)
+  res.render("profile",{user});
 });
 
 // router of register
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  postText: {
+  imageText: {
     type: String,
     required: true
   },
+  image: {
+    type: String,
+  },
+
   user:{
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
